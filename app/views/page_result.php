18b9d05cbb30eaef7f17d2f6726e547a614f445c
lCassieL <denisandwork@gmail.com>
2019-05-20 16:42:59 +0300

    added

--- app/views/page_result.php
+++ app/views/page_result.php
@@ -1,3 +1,2 @@
 <?php
-// var_dump($_SESSION['name'].' '.$_SESSION['surname'].' '.$_SESSION['phone'].'   '.$_SESSION['street'].' '.$_SESSION['house'].' '.$_SESSION['city'].'   '.$_SESSION['comment']);
-echo 'USER WAS ADDES';
+echo 'USER WAS ADDED';
